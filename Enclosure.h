57052910a911e81fcdd7565facc861b14535c90c
YoussefMoussa11002146 <ibyoyo68@gmail.com>
2025-11-12 15:14:09 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,27 @@
-#ifndef ENCLOSURE_H
-#define ENCLOSURE_H
-
+using namespace std;
+#include <stdlib.h>
+#include <string>
+#include <cmath>
 #include "Animal.h"
 
-//define enclosure class here 
+#ifndef FINALSEM_ENCLOSURE_H
+#define FINALSEM_ENCLOSURE_H
 
 
+class Enclosure{
+private:
+    int capacity;
+    int currentcount;
+    Animal* Animals;
 
 
+public:
+void displayAnimals();
+void addAnimal(Animal *a);
+Enclosure();
+Enclosure(int c,int curr, Animal a);
+~Enclosure();
+};
 
 
-
-
-#endif
+#endif //FINALSEM_ENCLOSURE_H
